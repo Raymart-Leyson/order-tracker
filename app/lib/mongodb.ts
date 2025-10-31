e438fa458c916c00b607f34a9e7f30cadf98ff89
Raymart-Leyson <raymart.leyson.rl@gmail.com>
2025-10-31 12:18:50 +0800

    Update mongodb

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,18 +1,27 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-if (!uri) throw new Error("MONGODB_URI not found");
+const uri = "mongodb+srv://ddrayleydd_db_user:aX84XBFIkRICCPYV@project1.ooihnzf.mongodb.net/project1?retryWrites=true&w=majority";
+
+if (!uri) {
+  throw new Error("MongoDB URI is missing!");
+}
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+// Use a global variable in development to prevent creating multiple connections
 if (process.env.NODE_ENV === "development") {
-  if (!(global as any)._mongoClientPromise) {
+  const globalWithMongo = global as typeof globalThis & {
+    _mongoClientPromise?: Promise<MongoClient>;
+  };
+
+  if (!globalWithMongo._mongoClientPromise) {
     client = new MongoClient(uri);
-    (global as any)._mongoClientPromise = client.connect();
+    globalWithMongo._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = globalWithMongo._mongoClientPromise;
 } else {
+  // In production, always create a new connection
   client = new MongoClient(uri);
   clientPromise = client.connect();
 }
